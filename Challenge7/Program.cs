e187eb903999765edf529bd382acf5e04ebeb26f
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-08-17 18:11:33 +0300

    Challenge 7 Completed

---
+++ Challenge7/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using Challenge7;
+
+int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+Result.StairCase(n);
---
+++ Challenge7/Result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge7
+{
+    internal class Result
+    {
+        public static void StairCase(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                string spaces = new string(' ', n - (i + 1));
+                string hashMarks = new string('#', i + 1);
+                Console.WriteLine(spaces + hashMarks);
+            }
+        }
+    }
+}
