bb6491f16a1388ec1684674123a8c36580543c97
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-08-17 17:18:26 +0300

    Challenge 4 Completed

---
+++ Challenge4/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//https://www.hackerrank.com/challenges/solve-me-first/problem challenge
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int val1 = Convert.ToInt32(Console.ReadLine());
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = SolveMeFirst(val1, val2);
+        Console.WriteLine(sum);
+        Console.ReadLine();
+    }
+
+    public static int SolveMeFirst(int a, int b)
+    {
+        if (a <= 1000 && a >= 1 && b <= 1000 && b >= 1)
+        {
+            return a + b;
+        }
+        return 0;
+    }
+}
+
+//Sample Input
+//val1 = 2
+//val2 = 3
+
+//Sample Output
+//5
