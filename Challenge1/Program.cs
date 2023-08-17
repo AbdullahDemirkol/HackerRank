897bfd038961326bf74c9b4c7be7de103067a57a
Abdullah Demirkol <55860994+AbdullahDemirkol@users.noreply.github.com>
2023-08-17 16:30:51 +0300

    Update Program.cs

--- Challenge1/Program.cs
+++ Challenge1/Program.cs
@@ -1,9 +1,9 @@
-﻿String inputString;
+//https://www.hackerrank.com/challenges/30-hello-world/problem challenge
+
+String inputString;
 
 inputString = Console.ReadLine();
 
 Console.WriteLine("Hello, World.");
 
 Console.WriteLine(inputString);
-
-//https://www.hackerrank.com/challenges/30-hello-world/problem challenge
