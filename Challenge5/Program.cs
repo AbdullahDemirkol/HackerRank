7341c81c9a82e1f781579ddfdf6a273b2509474e
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-08-17 17:44:41 +0300

    Challenge 5 Completed

---
+++ Challenge5/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//https://www.hackerrank.com/challenges/30-conditional-statements/problem challenge
+
+int n = Convert.ToInt32(Console.ReadLine().Trim());
+if (n % 2 == 0)
+{
+    Console.WriteLine("Not Weird");
+}
+else
+{
+    Console.WriteLine("Weird");
+}
+Console.ReadLine();
+
+
+//Sample Input 0
+//3
+
+//Sample Output 0
+//Weird
+
+//Sample Input 1
+//24
+
+//Sample Output 1
+//Not Weird
