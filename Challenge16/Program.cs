a326e29ae1fdad2fa5ce8c74cc26b3e53a15a3ef
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-08-18 19:51:50 +0300

    Challenge 16 Completed

---
+++ Challenge16/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+//https://www.hackerrank.com/challenges/30-recursion/problem challenge
+
+
+using Challenge16;
+
+int n = Convert.ToInt32(Console.ReadLine().Trim());
+if (n<=12 && n>=2) 
+{
+    int result = Result.Factorial(n);
+
+    Console.WriteLine(result);
+}
+
+
+//Sample Input
+//3
+
+//Sample Output
+//6                         => 3x2x1=6
---
+++ Challenge16/Result.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge16
+{
+    internal class Result
+    {
+
+        public static int Factorial(int n)
+        {
+            if (n <= 1) 
+                return 1;
+            
+            return n*Factorial(n-1);
+        }
+    }
+}
