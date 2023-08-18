a87e661450feb19548e14f3ad328adde34561298
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-08-18 20:32:45 +0300

    Challenge 17 and 18 Completed

---
+++ Challenge17/Program.cs
@@ -0,0 +1,31 @@
+﻿
+//https://www.hackerrank.com/challenges/30-exceptions-string-to-integer/problem challenge
+
+
+string S = Console.ReadLine();
+
+if (S.Length<=6 && S.Length >= 1)
+{
+	try
+	{
+		int result=Convert.ToInt32(S);
+		Console.WriteLine(result);
+	}
+	catch (Exception)
+	{
+        Console.WriteLine("Bad String");
+    }
+}
+
+
+//Sample Input 0
+//3
+
+//Sample Output 0
+//3
+
+//Sample Input 1
+//za
+
+//Sample Output 1
+//Bad String
---
+++ Challenge18/Program.cs
@@ -0,0 +1,44 @@
+﻿
+//https://www.hackerrank.com/challenges/30-review-loop/problem challenge
+
+int wordCount = Convert.ToInt32(Console.ReadLine());
+
+if (wordCount >= 1 && wordCount<=10)
+{
+    string[] words = new string[wordCount];
+    for (int i = 0; i < wordCount; i++)
+    {
+        string word = Console.ReadLine();
+        if (word.Length>=2 && word.Length<=10000)
+        {
+            words[i] = word;
+        }
+    }
+    foreach (string word in words)
+    {
+        string doubleDigit = null, firstDigit = null;
+        for (int i = 0;i < word.Length;i++)
+        {
+            if (i%2==0)
+            {
+                doubleDigit += word[i];
+            }
+            else
+            {
+                firstDigit += word[i];
+            }
+        }
+        Console.WriteLine(doubleDigit + " " + firstDigit);
+    }
+}
+
+
+
+//Sample Input
+//2
+//Hacker
+//Rank
+
+//Sample Output
+//Hce akr
+//Rn ak
