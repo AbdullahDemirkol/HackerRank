cb97ceedc781856c70a95f1cb63d7b4d36fc4084
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-08-18 21:39:48 +0300

    Challenge 21 Completed

---
+++ Challenge21/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+//https://www.hackerrank.com/challenges/30-generics/problem challenge
+
+using Challenge21;
+
+int n = Convert.ToInt32(Console.ReadLine());
+int[] intArray = new int[n];
+for (int i = 0; i < n; i++)
+{
+    intArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+n = Convert.ToInt32(Console.ReadLine());
+string[] stringArray = new string[n];
+for (int i = 0; i < n; i++)
+{
+    stringArray[i] = Console.ReadLine();
+}
+
+Result.PrintArray<Int32>(intArray);
+Result.PrintArray<String>(stringArray);
+
---
+++ Challenge21/Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge21
+{
+    internal class Result
+    {
+        public static void PrintArray<T>(T[] array)
+        {
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
