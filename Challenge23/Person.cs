80d6e7d937a0778b72a0c431f465f920ce69f1ba
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-08-19 10:39:13 +0300

    Challenge 23 Completed

---
+++ Challenge23/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge23
+{
+    internal class Person
+    {
+        public int age;
+        public Person(int initialAge)
+        {
+            age = initialAge;
+        }
+        public void amIOld()
+        {
+            if (age <0)
+            {
+                Console.WriteLine("Age is not valid, setting age to 0.");
+                age = 0;
+            }
+            if (age < 13 && age >= 0)
+            {
+                Console.WriteLine("You are young.");
+            }
+            else if (age < 18)
+            {
+                Console.WriteLine("You are a teenager.");
+            }
+            else
+            {
+                Console.WriteLine("You are old.");
+            }
+        }
+
+        public void yearPasses()
+        {
+            age++;
+        }
+    }
+}
---
+++ Challenge23/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+//https://www.hackerrank.com/challenges/30-class-vs-instance/problem challenge
+
+using Challenge23;
+
+int T = int.Parse(Console.In.ReadLine());
+for (int i = 0; i < T; i++)
+{
+    int age = int.Parse(Console.In.ReadLine());
+    Person person = new Person(age);
+    person.amIOld();
+    for (int j = 0; j < 3; j++)
+    {
+        person.yearPasses();
+    }
+    person.amIOld();
+    Console.WriteLine();
+}
+
+
+
+
+//Sample Input
+//4
+//-1
+//10
+//16
+//18
+
+//Sample Output
+//Age is not valid, setting age to 0.
+//You are young.
+//You are young.
+
+//You are young.
+//You are a teenager.
+
+//You are a teenager.
+//You are old.
+
+//You are old.
+//You are old.
