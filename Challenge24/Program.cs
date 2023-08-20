04fdcd8160fc83f86c8c1a9ae1b48b96dbcb5c68
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-08-21 00:12:08 +0300

    Challenge 24 Completed

---
+++ Challenge24/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+//https://www.hackerrank.com/challenges/diagonal-difference/problem?isFullScreen=true challenge
+
+
+
+using Challenge24;
+
+int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+List<List<int>> arr = new List<List<int>>();
+
+for (int i = 0; i < n; i++)
+{
+    arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+}
+
+int result = Result.DiagonalDifference(arr);
+
+Console.WriteLine(result);
+
+
+
+//Sample Input
+//3
+//11 2 4
+//4 5 6
+//10 8 -12
+
+//Sample Output
+//15        => 11+5+(-12)=4   4+5+10=19  |19-4|=15
---
+++ Challenge24/Result.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge24
+{
+    internal class Result
+    {
+
+        public static int DiagonalDifference(List<List<int>> arr)
+        {
+            int firstResult=0, secondResult=0;
+            var rowCount = arr[0].Count();
+            for (int i = 0; i < arr.Count(); i++)
+            {
+                for (int j = 0; j < arr[i].Count(); j++)
+                {
+                    if (i==j)
+                    {
+                        firstResult += arr[i][j];
+                    }
+                }
+                if (rowCount >= 0)
+                {
+                    rowCount--;
+                    secondResult += arr[i][rowCount];
+                }
+                Console.WriteLine(firstResult + " " + secondResult);
+            }
+            var result = firstResult - secondResult;
+            if (result < 0)
+            {
+                result*=-1;
+            }
+            return result;
+
+        }
+    }
+}
