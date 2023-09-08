6a616cadb89454defd96d910c7df274be55cbfe3
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-09-08 18:55:22 +0300

    C# Project 2 Created and Done

---
+++ Project#2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+//https://academy.patika.dev/tr/courses/c-projeleri/ucgen-cizme
+
+using Project_2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Üçgen Boyutunu Yazınız:");
+        var size = Convert.ToInt32(Console.ReadLine());
+        Triangle triangle = new Triangle(); 
+        triangle.DrawATriangle(size);
+    }
+}
---
+++ Project#2/Triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2
+{
+    internal class Triangle
+    {
+        internal void DrawATriangle(int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size-(i+1); j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int k = 0; k < (i*2)+1; k++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
