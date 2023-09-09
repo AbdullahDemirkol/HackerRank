c0c0d49e816ea1463b0b1d3c5bbee551117cb4cf
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-09-09 14:52:39 +0300

    C# Project 3 Created and Done

---
+++ Project#3/Circle.cs
@@ -0,0 +1,26 @@
+﻿internal class Circle
+{
+    internal void DrawACircle(int radius)
+    {
+        double thickness = 0.2;
+        double innerCircle = radius - thickness;
+        double outerCircle = radius + thickness;
+
+        for (double y = radius; y >= -radius; --y)
+        {
+            for (double x = -radius; x < outerCircle; x += 0.5)
+            {
+                double value = x * x + y * y;
+                if (value >= innerCircle * innerCircle && value <= outerCircle * outerCircle)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
