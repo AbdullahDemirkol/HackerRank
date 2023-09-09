caa06a1e0ee129abdd39d4a25e2c77eb8ec2221c
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-09-09 15:33:54 +0300

    C# Project 4 Created and Done

---
+++ Project#4/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+//https://academy.patika.dev/tr/courses/c-projeleri/string-ve-sayi-alan
+
+start:
+
+Console.WriteLine("Index ve ifade değerleri giriniz(Aralarını virgül ile ayırınız):");
+var statement=Console.ReadLine();
+
+if (statement != null)
+{
+    var statementElementArray = statement.Split(",");
+    if (statementElementArray != null && Convert.ToInt32(statementElementArray[1]) < statementElementArray[0].Length)
+    {
+        Console.WriteLine(statementElementArray[0].Remove(Convert.ToInt32(statementElementArray[1]), 1));
+    }
+    else
+    {
+        Console.WriteLine("Index değeri ifadeden daha büyük.");
+    }
+    goto start;
+}
+else
+{
+    Console.WriteLine("Hatalı yazı yazıldı.");
+}
