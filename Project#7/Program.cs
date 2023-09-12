e1ea262a7a7793e3abefcdc9c994ed7b31425b67
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-09-12 22:46:06 +0300

    C# Project 7 Created and Done

---
+++ Project#7/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+//https://academy.patika.dev/tr/courses/c-projeleri/integer-ikili-toplami
+
+Console.WriteLine("Sayıları Boşluk Bırakarak Giriniz:");
+var numbers = Console.ReadLine();
+var numbersArray = numbers.Split(' ');
+for (int i = 0; i < numbersArray.Length; i += 2)
+{
+    var firstNumber = numbersArray[i];
+    var secondNumber= numbersArray[i+1];
+    if (firstNumber!=secondNumber)
+    {
+        Console.Write((int.Parse(firstNumber) + int.Parse(secondNumber))+" ");
+    }
+    else
+    {
+        Console.Write(Math.Pow(int.Parse(firstNumber) + int.Parse(secondNumber), 2)+ " ");
+    }
+}
+
+
+
+
+//Örnek Input: 2 3 1 5 2 5 3 3
+
+//Output: 5 6 7 81
