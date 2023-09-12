4f72cc21b09b405381eec06e10c6a3235358c78a
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-09-13 00:13:45 +0300

    C# Project 8 Created and Done

---
+++ Project#8/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+//https://academy.patika.dev/tr/courses/c-projeleri/mutlak-kare
+
+
+Console.WriteLine("Aralarında Boşluk Bırakarak Sayılar Giriniz:");
+var numbers = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+double smallResult = 0;
+double bigResult = 0;
+foreach (var number in numbers)
+{
+    if (number < 67)
+    {
+        smallResult += (67-number);
+    }
+    else if (number > 67)
+    {
+        bigResult += Math.Pow((number - 67), 2);
+    }
+}
+Console.Write(smallResult + " " + bigResult);
+
+
+
+//Örnek: Input: 56 45 68 77
+//Output: 33 101
