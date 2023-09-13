d3fe02019c34b650b7f8cef7479f18abc5b4ca31
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-09-13 16:17:23 +0300

    C# Project 9 Created and Done

---
+++ Project#9/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+//https://academy.patika.dev/tr/courses/c-projeleri/karakter-degistirme
+
+
+Console.WriteLine("Cümlenizi Giriniz:");
+string[] stringInputs = Console.ReadLine().Split(' ');
+if (stringInputs.Count() != 0)
+{
+    string newString;
+    foreach (string stringInput in stringInputs)
+    {
+        if (stringInput.Length > 1)
+        {
+            newString = "";
+            newString += stringInput.ToCharArray()[stringInput.Length - 1];
+            for (int i = 1; i < stringInput.Length - 1; i++)
+            {
+                newString += stringInput[i];
+            }
+            newString += stringInput[0];
+            Console.Write(newString + " ");
+        }
+        else if (stringInput.Length == 1) { Console.Write(stringInput+" "); }
+    }
+}
+
+//Örnek: Input: Merhaba Hello Algoritma x
+//Output: aerhabM oellH algoritmA x
