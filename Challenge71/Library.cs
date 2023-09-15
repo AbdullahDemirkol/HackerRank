316f94fcd1c77c92f47645d75ed850e357f3a2fb
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-09-16 00:55:52 +0300

    Challenge 71 Completed

---
+++ Challenge71/Library.cs
@@ -0,0 +1,36 @@
+﻿internal class Library
+{
+    internal static int CheckDate(int d1, int d2, int m1, int m2, int y1, int y2)
+    {
+        if (y2 < y1)
+        {
+            return 10000;
+        }
+        else if (y1<y2)
+        {
+            return 0;
+        }
+        else 
+        {
+            if (m2 < m1)
+            {
+                return (m1 - m2) * 500;
+            }
+            else if (m1<m2)
+            {
+                return 0;
+            }
+            else
+            {
+                if (d2 < d1)
+                {
+                    return (d1 - d2) * 15;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+}
---
+++ Challenge71/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+//https://www.hackerrank.com/challenges/30-nested-logic/problem?isFullScreen=true challenge
+
+
+var a = Console.ReadLine().Split(' ').ToList().Select(x=>Convert.ToInt32(x)).ToList();
+
+var d1 = a[0];
+var m1 = a[1];
+var y1 = a[2];
+
+a = Console.ReadLine().Split(' ').ToList().Select(x => Convert.ToInt32(x)).ToList();
+
+var d2 = a[0];
+var m2 = a[1];
+var y2 = a[2];
+
+var result = Library.CheckDate(d1,d2,m1,m2,y1,y2);
+
+Console.WriteLine(result);
+
+
+
+//Sample Input
+//9 6 2015    day = 9, month = 6, year = 2015(date returned)
+//6 6 2015    day = 6, month = 6, year = 2015(date due)
+
+//Sample Output
+//45
