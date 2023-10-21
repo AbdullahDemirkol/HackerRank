ac508cca6347b82021fa347db6ac55d8bfe00fd5
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-10-21 04:54:39 +0300

    Challenge 73 Completed

---
+++ Challenge73/Program.cs
@@ -0,0 +1,36 @@
+﻿
+//https://www.hackerrank.com/challenges/30-bitwise-and/problem?isFullScreen=true challenge
+
+using Challenge73;
+
+
+int t = Convert.ToInt32(Console.ReadLine().Trim());
+var result = new List<int>();
+
+for (int tItr = 0; tItr < t; tItr++)
+{
+    string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+    int count = Convert.ToInt32(firstMultipleInput[0]);
+
+    int lim = Convert.ToInt32(firstMultipleInput[1]);
+
+    int res = Result.BitwiseAnd(count, lim);
+
+    result.Add(res);
+}
+foreach (var resul in result)
+{
+    Console.WriteLine(resul);
+}
+
+//Sample Input
+//3       =>  T = 3
+//5 2     =>  N = 5, K = 2
+//8 5     =>  N = 8, K = 5
+//2 2     =>  N = 8, K = 5
+
+//Sample Output
+//1
+//4
+//0
---
+++ Challenge73/Result.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge73
+{
+    internal class Result
+    {
+        public static int BitwiseAnd(int N, int K)
+        {
+            var max = int.MinValue;
+            for (int i = 1; i < N; i++)
+            {
+                for (int j = i + 1; j <= N; j++)
+                {
+                    var cur = i & j;
+                    if (cur <= K)
+                    {
+                        max = Math.Max(max, cur);
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}
