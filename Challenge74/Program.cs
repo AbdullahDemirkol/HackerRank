33978a33c875f2fc3622eccd0da15501f11056f4
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-10-21 06:25:06 +0300

    Challenge74 Completed

---
+++ Challenge74/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+//https://www.hackerrank.com/challenges/sherlock-and-the-beast/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=7-day-campaign challenge
+
+
+
+using Challenge74;
+using static System.Net.Mime.MediaTypeNames;
+
+int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+for (int tItr = 0; tItr < t; tItr++)
+{
+    int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+    Result.DecentNumber(n);
+}
+
+//Sample Input
+//4       t = 4
+//1       n = 1 (first test case)
+//3       n = 3(second test case)
+//5
+//11
+
+//Sample Output
+//-1
+//555
+//33333
+//55555533333
---
+++ Challenge74/Result.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge74
+{
+    internal class Result
+    {
+        internal static void DecentNumber(int n)
+        {
+            var result = string.Empty;
+            if (n < 3 || n == 4 || n == 7)
+            {
+                Console.WriteLine("-1");
+                return;
+            }
+
+            while (n % 3 != 0)
+            {
+                result += "33333";
+                n -= 5;
+            }
+
+            while (n > 0)
+            {
+                result = "555" + result;
+                n -= 3;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
