5efcc902dd27664d59ed9af7400c0e5f3b854239
Abdullah Demirkol <abdullahdemirkol01@gmail.com>
2023-10-21 07:12:23 +0300

    Challenge 75 Completed

---
+++ Challenge75/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+//https://www.hackerrank.com/challenges/halloween-party/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign challenge
+
+
+using Challenge75;
+
+int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+for (int tItr = 0; tItr < t; tItr++)
+{
+    int k = Convert.ToInt32(Console.ReadLine().Trim());
+
+    long result = Result.HalloweenParty(k);
+
+    Console.WriteLine(result);
+}
+
+
+//Sample Input
+//4
+//5
+//6
+//7
+//8
+
+//Sample Output 
+//6
+//9
+//12
+//16
---
+++ Challenge75/Result.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge75
+{
+    internal class Result
+    {
+        internal static long HalloweenParty(int k)
+        {
+            decimal halfTheNumber = (decimal)k / 2;
+            int horizontalCount = (int)halfTheNumber;
+            var verticalCount = (int)(halfTheNumber+(halfTheNumber-horizontalCount));
+            
+            long result = 0;
+            int count = 0;
+
+            while (verticalCount != 0)
+            {
+                if ((verticalCount & 1) == 1)
+                {
+                    result += (long)horizontalCount << count;
+                }
+                verticalCount >>= 1;
+                count++;
+            }
+
+            return result;
+        }
+    }
+}
